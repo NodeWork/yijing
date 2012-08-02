404b3cfff55cc9e421d962e8530a28f570e5457c
Haisheng.W.WU <freizl@gmail.com>
2012-08-02 18:56:59 +0800

    misc change

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,15 @@
 var defaultGua = '111111';
 
 exports.index = function(req, res) {
-  res.render('index', { guaname: ''})
+    res.render('index', { guaname: ''});
 };
 
 exports.gua = function(req, res) {
     var param = req.params.guakey || req.query.guakey || defaultGua ;
-
-  res.render('search', { guaname: param })
+    res.render('search', { guaname: param });
 };
 
 exports.doc = function(req, res) {
     var docname = req.params.docname;
-
     res.render(docname);
 };
