4899b8c25fc1426a297402c43f89d357db6e01b0
Haisheng.W.WU <freizl@gmail.com>
2014-02-26 10:10:09 -0500

    style for home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,6 @@ angular.module('yijing')
   .controller('MainCtrl', ['$scope', 'guaService', function ($scope, guaService) {
 
      $scope.itemss = guaService.getSixFourGuas();
-     console.log($scope.itemss);
+     //console.log($scope.itemss);
 
   }]);
